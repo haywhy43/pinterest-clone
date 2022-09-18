6f22afa91cc326c48065cd761e774e0dbb172ec2
Ayomikun <ayomikun@mypatricia.co>
2022-09-18 19:12:31 +0100

    updated text to dev

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>We are learning React</p>
+        <p>We are learning React.... this is dev</p>
         <a
           className="App-link"
           href="https://reactjs.org"
